5d95f7a0335045de3c45b1d749f3d750b8354cbf
Gabreilpinaz <pinagabriel2017@gmail.com>
2022-11-06 11:57:37 -0300

    Bean Validation

---
+++ src/main/java/com/bielpina/dscommerce/dto/ValidationError.java
@@ -0,0 +1,22 @@
+package com.bielpina.dscommerce.dto;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends CustomError {
+
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public ValidationError(Instant timestamp, Integer status, String error, String path) {
+        super(timestamp, status, error, path);
+    }
+
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+    public void addError (String fieldNme, String message) {
+        errors.add(new FieldMessage(fieldNme, message));
+    }
+}
