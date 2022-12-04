05a8bb4f22671790f56343d4f9d1d2ab224a28e2
Gabreilpinaz <pinagabriel2017@gmail.com>
2022-12-04 13:13:30 -0300

    Getting user logged

--- src/main/java/com/bielpina/dscommerce/services/UserService.java
+++ src/main/java/com/bielpina/dscommerce/services/UserService.java
@@ -1,12 +1,15 @@
 package com.bielpina.dscommerce.services;
 
+import com.bielpina.dscommerce.dto.UserDTO;
 import com.bielpina.dscommerce.entities.User;
 import com.bielpina.dscommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -22,5 +25,20 @@ public class UserService implements UserDetailsService {
             throw new UsernameNotFoundException("Email not found");
         }
         return user;
+    }
+
+    protected User authenticated() {
+        try {
+            String username = SecurityContextHolder.getContext().getAuthentication().getName();
+            return repository.findByEmail(username);
+        }
+        catch (Exception e) {
+            throw new UsernameNotFoundException("Invalid user");
+        }
+    }
+    @Transactional
+    public UserDTO getMe() {
+        User entity = authenticated();
+        return new UserDTO(entity);
     }
 }
